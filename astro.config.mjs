1679dde1c4f1cb1030b069021a0d9bd77d69d46f
whwen <954433942@qq.com>
2024-11-30 15:15:50 +0000

    Set up Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://whwen.github.io',
+  integrations: [
+    starlight({
+      title: '我的令人愉悦的文档网站',
+    }),
+  ],
+});
