18603d3a7c4d2d3cd0f484839bd1193057afaea1
whw <whw@whw.com>
2024-12-01 18:44:51 +0800

    Add starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
-// @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    title: 'Whwen',
+    integrations: [
+      //https://starlight.astro.build/zh-cn/reference/configuration/
+      starlight({
+        title: 'Whwen',
+        components: {
+//           Head: './src/components/MainHead.astro',
+//           Header: './src/components/Nav.astro',
+//           ThemeSelect: './src/components/ThemeToggle.astro',
+        },
+        customCss: [
+          // 你的自定义 CSS 文件的相对路径
+//           './src/styles/global.css',
+        ],
+      }),
+    ],
+});
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,9 @@
 import { defineCollection, z } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	work: defineCollection({
 		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
+		schema: docsSchema(),
 	}),
 };
